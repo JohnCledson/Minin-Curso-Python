02e3617a223ea72e6484cbb0579b4621917c44f9
John Cledson <cledsonjohn123@gmail.com>
2022-11-01 20:01:51 -0300

    Faturamento por Loja

--- venv/MeuArquivo.py
+++ venv/MeuArquivo.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
 # Importar a base de dados
-tabela_vendas = pd.read_excel('Vendas.xlsx')
+tabela_vendas = pd.read_excel('venv/Vendas.xlsx')
 
 # Visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
 
 # Faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
 
 # Quantidade de produtos vendidos por loja
 
