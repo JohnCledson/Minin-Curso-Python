b31346e1c445c23e8868f959266b7e2bfab23354
John Cledson <cledsonjohn123@gmail.com>
2022-11-01 08:27:06 -0300

    first commit

---
+++ venv/MeuArquivo.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+print(tabela_vendas)
+
+# Visualizar a base de dados
+
+# Faturamento por loja
+
+# Quantidade de produtos vendidos por loja
+
+# Ticket medio por produto em cada loja
+
+# Envia um email com o relatório
